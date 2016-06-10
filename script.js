28f00e5d840574739597bc052ffc50ab43793635
Christian Mansour <c.mansour@miami.edu>
2016-06-10 14:25:23 -0500

    first commit basic html/css

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  //e.preventDefault();
+  $('td').click(function(){
+    //alert('click');
+    //When box is clicked have return true
+    if ($(this) === $('td')) {
+      alert('yay');
+    }
+
+  });
+
+
+
+
+//When box is clicked, return value
+//Switch players
+//when box is clicked, return value. if value is same as box previously clicked, don't allow.
+//when 3 boxes in a row are true, return player wins
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
